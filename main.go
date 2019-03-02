f5323d30e5cccdb253d2ecc094033648a6d86982
Adron Hall <adron@thrashingcode.com>
2019-03-02 11:16:26 -0800

    Example of channel synchronization.

--- main.go
+++ main.go
@@ -1,42 +1,32 @@
-// _Channels_ are the pipes that connect concurrent
-// goroutines. You can send values into channels from one
-// goroutine and receive those values into another
-// goroutine.
-// Example from: https://gobyexample.com/channels
+// We can use channels to synchronize execution
+// across goroutines. Here's an example of using a
+// blocking receive to wait for a goroutine to finish.
 
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+import "time"
+
+// This is the function we'll run in a goroutine. The
+// `done` channel will be used to notify another
+// goroutine that this function's work is done.
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second * 3)
+	fmt.Println("done")
+
+	// Send a value to notify that we're done.
+	done <- true
+}
 
 func main() {
 
-	// Create a new channel with `make(chan val-type)`.
-	// Channels are typed by the values they convey.
-	messages := make(chan string)
+	// Start a worker goroutine, giving it the channel to
+	// notify on.
+	done := make(chan bool, 1)
+	go worker(done)
 
-	// _Send_ a value into a channel using the `channel <-`
-	// syntax. Here we send `"ping"`  to the `messages`
-	// channel we made above, from a new goroutine.
-	go func() {
-		messages <- "ping"
-		time.Sleep(time.Second * 2)
-		messages <- "We had our anonymous function complete!"
-	}()
-
-	// The `<-channel` syntax _receives_ a value from the
-	// channel. Here we'll receive the `"ping"` message
-	// we sent above and print it out.
-	msg := <-messages
-
-	fmt.Println(msg)
-	fmt.Scanln()
-
-	// Here time has elapsed and the messages channel has a new
-	// value in it. So we ask to receive the latest message
-	// and print it out.
-	msg = <-messages
-	fmt.Println(msg)
+	// Block until we receive a notification from the
+	// worker on the channel.
+	<-done
 }
